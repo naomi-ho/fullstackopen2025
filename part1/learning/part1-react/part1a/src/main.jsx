db1b63621642d12600e9e9749f1e4e206942a7d7
naomi-ho <151114635+naomi-ho@users.noreply.github.com>
2025-10-22 23:04:12 -0400

    Intro to React

---
+++ part1/learning/part1-react/part1a/src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+
+import App from './App.jsx';
+
+// renders contents into the div-element, defined in file index.html, having id value 'root'
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);

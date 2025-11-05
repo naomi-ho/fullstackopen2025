b6227b21cc22104ea169a902ecf1d878dfcbe17c
naomi-ho <151114635+naomi-ho@users.noreply.github.com>
2025-11-05 15:58:44 -0500

    Use object variable definitions for App component and refactor app so it still works

--- part1/submissions/part1/courseinfo/src/App.jsx
+++ part1/submissions/part1/courseinfo/src/App.jsx
@@ -6,59 +6,55 @@ const Part = (content) => {
   return (
     <>
       <p>
-        {content.part} {content.exercises}
+        {content.part.name} {content.part.exercises}
       </p>
     </>
   );
 };
 
-const Content = (parts) => {
-  return (
-    <>
-      <Part part={parts.parts[0].name} exercises={parts.parts[0].exercises} />
-      <Part part={parts.parts[1].name} exercises={parts.parts[1].exercises} />
-      <Part part={parts.parts[2].name} exercises={parts.parts[2].exercises} />
-    </>
-  );
-};
+// const Content = (parts) => {
+//   return (
+//     <>
+//       <Part part={parts.part1.name} exercises={parts.parts.exercises} />
+//       <Part part={parts.parts[1].name} exercises={parts.parts[1].exercises} />
+//       <Part part={parts.parts[2].name} exercises={parts.parts[2].exercises} />
+//     </>
+//   );
+// };
 
 const Total = (parts) => {
   return (
     <>
       <p>
         Number of exercises{' '}
-        {parts.parts[0].exercises +
-          parts.parts[1].exercises +
-          parts.parts[2].exercises}
+        {parts.part1.exercises + parts.part2.exercises + parts.part3.exercises}
       </p>
     </>
   );
 };
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      },
-    ],
+  const course = 'Half Stack application development';
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
   };
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Header course={course} />
+      <Part part={part1} />
+      <Part part={part2} />
+      <Part part={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
     </div>
   );
 };
